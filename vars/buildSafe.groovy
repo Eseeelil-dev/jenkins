5552cea85ec6c84f8f840eeab2be47bf3869a4ff
Eseeelil-dev <eseelil@gmail.com>
2023-02-07 22:03:22 +0300

    Tee1

--- vars/buildSafe.groovy
+++ vars/buildSafe.groovy
@@ -1,10 +1,8 @@
 def call(Map config) {
-    node {
-        try {
-            build job: config.job, parameters: config.parameters
-        }
-        catch (hudson.AbortException e) {
-            echo "Some error is happened ${e}"
-        }
+    try {
+        build job: config.job, parameters: config.parameters
+    }
+    catch (hudson.AbortException e) {
+        echo "Some error is happened ${e}"
     }
 }
