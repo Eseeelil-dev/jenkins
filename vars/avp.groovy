337af13f53bda25915444f7eaf1b223780a4b1d7
Eseeelil-dev <eseelil@gmail.com>
2023-02-08 23:18:49 +0300

    TTqq1

--- vars/avp.groovy
+++ vars/avp.groovy
@@ -19,9 +19,11 @@ def enableTest() {
   * @param hosts List of hosts to cleanup
   */
 def cleanupHosts(List<String> hosts) {
-    hosts.each { host -> 
-        sh label: "Cleanup ${host}", script: """#!/bin/bash +x
-            rm -rf /tmp/* || true
-        """
+    hosts.each { host ->
+        sshagent(credentials: ['user-creds'], ignoreMissing: true) {
+            sh label: "Cleanup ${host}", script: """#!/bin/bash +x
+                rm -rf /tmp/* || true
+            """
+        }
     }
 }
