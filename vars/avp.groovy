829583f318df8aac994ef6f42cd1808ff8a7c676
Eseeelil-dev <eseelil@gmail.com>
2023-02-08 22:32:15 +0300

    Var1

--- vars/avp.groovy
+++ vars/avp.groovy
@@ -1,8 +1,13 @@
-def call(Map config) {
+def build(Map config) {
     try {
         build job: config.job, parameters: config.parameters
     }
     catch (hudson.AbortException e) {
         echo "Some error is happened ${e}"
     }
+}
+
+def enableTest() {
+    echo 'Enable the tests'
+    sh 'env | sort'
 }
